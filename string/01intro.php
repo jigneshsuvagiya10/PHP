ee287ba5f40580e51812247eade83beebab093d0
jigneshsuvagiya10 <jigneshsuvagiya10@gmail.com>
2022-12-20 11:12:55 +0530

    string

---
+++ string/01intro.php
@@ -0,0 +1,20 @@
+<?php
+
+$str ="Something 123";
+$str1 ='Something#$%35 ';
+
+echo $str1;
+echo "<br>";
+echo $str1[0];
+echo "<br>";
+echo $str1[1];
+
+
+$name = "Test";
+echo "<br>";
+echo 'Name is $name in single qouate';
+echo "<br>";
+echo 'Name is '.$name.' in single qouate';
+echo "<br>";
+echo "Name is $name in double qouate";
+?>
---
+++ string/02addslash.php
@@ -0,0 +1,10 @@
+<?php
+
+$s = "Welcome to 't tops' tecno";
+
+echo $s = "this is jay's id";
+echo "<br>";
+
+echo $s = addslashes("this is jay's id");
+
+?>
